71bb7e84af62471dab08066740b06d30293a0d83
Mariya Katayeva <mariya.katayeva@phystech.edu>
2024-02-26 00:21:27 +0300

    add bar for mi, add text description below each chart

--- results_parser.py
+++ results_parser.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def make_bar(fig, values, labels: list, title: str, xlabel: str, ylabel: str):
+def make_bar(fig, values, labels: list, title: str, xlabel: str, ylabel: str, bottom=0.0):
     make_stacked_bars(
         fig,
         labels=labels,
@@ -11,7 +11,8 @@ def make_bar(fig, values, labels: list, title: str, xlabel: str, ylabel: str):
         categories=("",),
         title=title,
         xlabel=xlabel,
-        ylabel=ylabel
+        ylabel=ylabel,
+        bottom=bottom
     )
 
 
@@ -22,7 +23,8 @@ def make_stacked_bars(
         categories: tuple,
         title: str,
         xlabel: str,
-        ylabel: str
+        ylabel: str,
+        bottom=0.0
 ):
     plt.title(title)
     plt.xlabel(xlabel)
@@ -30,7 +32,7 @@ def make_stacked_bars(
     plt.grid()
     plt.xticks(visible=False)  # don't show x labels to avoid overlap
     plt_bars = []
-    bottom = np.zeros(len(labels))
+    bottom = np.repeat(bottom, len(labels))
     for bar_values, category in zip(values, categories):
         plt_bars.append(
             plt.bar(
@@ -86,6 +88,19 @@ def make_stacked_bars(
     fig.canvas.mpl_connect('pick_event', on_pick)
 
 
+def make_plot_description(text: str):
+    plt.annotate(
+        text=text,
+        xy=(0, 0),
+        xytext=(0, -20),
+        xycoords='axes fraction',
+        textcoords='offset points',
+        va='top',
+        annotation_clip=True,
+        bbox=dict(boxstyle="square", facecolor='orange', alpha=0.5)
+    )
+
+
 def sort(values: list, labels: list):
     values_and_labels = sorted(zip(values, labels), reverse=True)
     return zip(*values_and_labels)
@@ -99,9 +114,9 @@ def cc_parser(data: dict):
             complexities.append(entity["complexity"])
             cc_info.append(
                 f'name: {entity["name"]}\n'
-                f'type: {entity["type"]}\n' 
-                f'rank: {entity["rank"]}\n' 
-                f'complexity: {entity["complexity"]}\n' 
+                f'type: {entity["type"]}\n'
+                f'rank: {entity["rank"]}\n'
+                f'complexity: {entity["complexity"]}\n'
                 f'filename: {filename}'
             )
     complexities, cc_info = sort(complexities, cc_info)
@@ -113,6 +128,7 @@ def cc_parser(data: dict):
         xlabel="Blocks",
         ylabel="Complexity"
     )
+    make_plot_description(f"Total blocks count: {len(cc_info)}")
     plt.show()
 
 
@@ -149,6 +165,8 @@ def hal_parser(data: dict):
         xlabel='Files',
         ylabel='Difficulty'
     )
+    make_plot_description(f"Total files count: {len(difficulties_files)}\n"
+                          f"Nonzero halstead metric in {np.count_nonzero(difficulties_files)} files")
     plt.subplot(2, 1, 2)
     make_bar(
         fig,
@@ -158,6 +176,8 @@ def hal_parser(data: dict):
         xlabel='Functions',
         ylabel='Difficulty'
     )
+    make_plot_description(f"Total functions count: {len(difficulties_funcs)}\n"
+                          f"Nonzero halstead metric in {np.count_nonzero(difficulties_funcs)} functions")
     plt.show()
 
 
@@ -192,19 +212,40 @@ def raw_parser(data: dict):
         fig,
         values=(sloc, oneline_strings, multiline_strings, blank),
         labels=labels,
-        categories=("SLOC", "oneline docstrings", "multiline docstrings", "blank"),
+        categories=("SLOC", "oneline comments and docstrings", "multiline docstrings", "blank"),
         title="Raw",
         xlabel="files",
         ylabel="lines of code"
     )
-    plt.legend()
+    fig.legend()
+    make_plot_description(f"Total files count: {len(files)}\n"
+                          f"LOC: {sum(loc)}\n"
+                          f"SLOC: {sum(sloc)}\n"
+                          f"Oneline comments and docstrings: {sum(oneline_strings)}\n"
+                          f"Multiline docstrings: {sum(multiline_strings)}\n"
+                          f"Blank: {sum(blank)}")
     plt.show()
 
     fig = plt.figure('Statistics', figsize=(10, 5), frameon=True, layout="constrained")
     plt.subplot(2, 1, 1)
     make_bar(fig, labels=labels, values=lloc, title="Logical lines of code", xlabel="files", ylabel="lines of code")
+    make_plot_description(f"Total files count: {len(files)}\n"
+                          f"LLOC: {sum(lloc)}")
     plt.subplot(2, 1, 2)
     make_bar(fig, labels=labels, values=comments, title="Comments", xlabel="files", ylabel="lines of code")
+    make_plot_description(f"Total files count: {len(files)}\n"
+                          f"Comments: {sum(comments)}")
+    plt.show()
+
+
+def mi_parser(data: dict):
+    descriptions, mi = zip(*[(f"file: {k}\nmi: {v['mi']}%", v["mi"]) for k, v in data.items()])
+    inverted_mi = np.array(mi) - 100
+    fig = plt.figure('Maintainability index', figsize=(10, 5), frameon=True, layout="constrained")
+    make_bar(fig, values=inverted_mi, labels=descriptions, title="MI", xlabel="files", ylabel="Percentage", bottom=100.0)
+    plot_description = (f"Total files count: {len(descriptions)}\n" 
+                        f"MI≠100% in {np.count_nonzero(inverted_mi)} files")
+    make_plot_description(plot_description)
     plt.show()
 
 
@@ -216,3 +257,6 @@ with open("cc_results.json", "r") as f:
 
 with open("hal_results.json", "r") as f:
     hal_parser(json.loads(f.read()))
+
+with open("mi_results.json", "r") as f:
+    mi_parser(json.loads(f.read()))
