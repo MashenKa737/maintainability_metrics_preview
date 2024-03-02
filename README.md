28c3723af5e7ce66dfe55c26f260fdb369d35430
Mariya Katayeva <mariya.katayeva@phystech.edu>
2024-03-02 22:52:04 +0300

    add script to run radon and then draw results

--- README.md
+++ README.md
@@ -5,18 +5,11 @@
 ## Getting started
 
 install radon and matplotlib
-run in current folder:
+run script:
 
 ```bash
-radon cc <path to analyzed project src> -j > cc_results.json
-radon hal <path to analyzed project src> -j > hal_results.json
-radon raw <path to analyzed project src> -j > raw_results.json
-radon mi <path to analyzed project src> -j > mi_results.json
+python radon_preview.py <path_to_analyzed_projecte>
 ```
 
-run script
-
-```bash
-python results_parser.py
-```
 The result will be matplotlib bar charts, opening one by one, when close previous window. Each chart will open selected bar description, if click on bar rectangle.
+If use option '-s', save charts to the output folder (default './')
--- results_parser.py
+++ results_parser.py
@@ -1,9 +1,13 @@
-import json
+# import json
+import os.path
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def make_bar(fig, values, labels: list, title: str, xlabel: str, ylabel: str, bottom=0.0):
+def make_bar(
+    fig, values, labels: list, title: str, xlabel: str, ylabel: str, bottom=0.0
+):
     make_stacked_bars(
         fig,
         labels=labels,
@@ -12,19 +16,19 @@ def make_bar(fig, values, labels: list, title: str, xlabel: str, ylabel: str, bo
         title=title,
         xlabel=xlabel,
         ylabel=ylabel,
-        bottom=bottom
+        bottom=bottom,
     )
 
 
 def make_stacked_bars(
-        fig,
-        labels: list,
-        values: tuple,
-        categories: tuple,
-        title: str,
-        xlabel: str,
-        ylabel: str,
-        bottom=0.0
+    fig,
+    labels: list,
+    values: tuple,
+    categories: tuple,
+    title: str,
+    xlabel: str,
+    ylabel: str,
+    bottom=0.0,
 ):
     plt.title(title)
     plt.xlabel(xlabel)
@@ -38,32 +42,32 @@ def make_stacked_bars(
             plt.bar(
                 x=labels,
                 height=bar_values,
-                align='edge',
+                align="edge",
                 picker=True,  # show bar annotation on the pick event
                 label=category,
-                bottom=bottom
+                bottom=bottom,
             )
         )
         bottom += np.array(bar_values)
     plt_labels = []
 
     text_kwargs = dict(
-        ha='left',
-        va='top',
-        color='C1',
-        size='small',
-        bbox=dict(facecolor='white', alpha=0.8),
-        visible=False  # all annotations are hidden until being picked
+        ha="left",
+        va="top",
+        color="C1",
+        size="small",
+        bbox=dict(facecolor="white", alpha=0.8),
+        visible=False,  # all annotations are hidden until being picked
     )
     for index, rect in enumerate(plt_bars[-1].patches):
         text = plt.annotate(
             labels[index],
             xy=(rect.get_x(), rect.get_y() + rect.get_height()),
             xytext=(50, -50),
-            textcoords='offset pixels',
-            arrowprops=dict(arrowstyle='->'),
+            textcoords="offset pixels",
+            arrowprops=dict(arrowstyle="->"),
             annotation_clip=True,
-            **text_kwargs
+            **text_kwargs,
         )
         plt_labels.append(text)
 
@@ -80,12 +84,14 @@ def make_stacked_bars(
             picked_bar = next(b for b in plt_bars if bar in b.patches)
             bar_index = picked_bar.index(bar)
             print(labels[bar_index])
-            plt_labels[bar_index].set_visible(True)  # show annotation for the selected bar
+            plt_labels[bar_index].set_visible(
+                True
+            )  # show annotation for the selected bar
             visible_labels.append(plt_labels[bar_index])
 
             fig.canvas.draw()
 
-    fig.canvas.mpl_connect('pick_event', on_pick)
+    fig.canvas.mpl_connect("pick_event", on_pick)
 
 
 def make_plot_description(text: str):
@@ -93,17 +99,17 @@ def make_plot_description(text: str):
         text=text,
         xy=(0, 0),
         xytext=(0, -20),
-        xycoords='axes fraction',
-        textcoords='offset points',
-        va='top',
+        xycoords="axes fraction",
+        textcoords="offset points",
+        va="top",
         annotation_clip=True,
-        bbox=dict(boxstyle="square", facecolor='orange', alpha=0.5)
+        bbox=dict(boxstyle="square", facecolor="orange", alpha=0.5),
     )
 
 
-def save_or_show(save: bool, filename: str):
-    if save:
-        plt.savefig(filename)
+def save_or_show(save_output: str, filename: str):
+    if save_output:
+        plt.savefig(os.path.join(save_output, filename))
         plt.close()
     else:
         plt.show()
@@ -114,7 +120,7 @@ def sort(values: list, labels: list):
     return zip(*values_and_labels)
 
 
-def cc_parser(data: dict, save=False):
+def cc_parser(data: dict, save_output: str = None):
     complexities = []
     cc_info = []
     for filename in data.keys():
@@ -125,71 +131,79 @@ def cc_parser(data: dict, save=False):
                 f'type: {entity["type"]}\n'
                 f'rank: {entity["rank"]}\n'
                 f'complexity: {entity["complexity"]}\n'
-                f'filename: {filename}'
+                f"filename: {filename}"
             )
     complexities, cc_info = sort(complexities, cc_info)
     make_bar(
-        plt.figure("Cyclomatic complexity", figsize=(12, 3), frameon=True, layout="constrained"),
+        plt.figure(
+            "Cyclomatic complexity", figsize=(12, 3), frameon=True, layout="constrained"
+        ),
         values=complexities,
         labels=cc_info,
         title="Cyclomatic complexity",
         xlabel="Blocks",
-        ylabel="Complexity"
+        ylabel="Complexity",
     )
     make_plot_description(f"Total blocks count: {len(cc_info)}")
-    save_or_show(save, 'cc.png')
+    save_or_show(save_output, "cc.png")
 
 
-def hal_parser(data: dict, save=False):
+def hal_parser(data: dict, save_output: str = None):
     difficulties_files = []
     summaries_files = []
     difficulties_funcs = []
     summaries_funcs = []
 
     for filename in data.keys():
-        total_entry = data[filename]['total']
-        difficulties_files.append(total_entry['difficulty'])
+        total_entry = data[filename]["total"]
+        difficulties_files.append(total_entry["difficulty"])
         summaries_files.append(
-            f"filename: {filename}\n" +
-            "\n".join([f'{key}: {value}' for key, value in total_entry.items()])
+            f"filename: {filename}\n"
+            + "\n".join([f"{key}: {value}" for key, value in total_entry.items()])
         )
-        func_entries = data[filename]['functions']
+        func_entries = data[filename]["functions"]
         for func_name in func_entries.keys():
             entry = func_entries[func_name]
-            difficulties_funcs.append(entry['difficulty'])
+            difficulties_funcs.append(entry["difficulty"])
             summaries_funcs.append(
-                f"function: {func_name}\n" +
-                f"filename: {filename}\n" +
-                "\n".join([f'{key}: {value}' for key, value in entry.items()])
+                f"function: {func_name}\n"
+                + f"filename: {filename}\n"
+                + "\n".join([f"{key}: {value}" for key, value in entry.items()])
             )
 
-    fig = plt.figure('Halstead metric', figsize=(10, 5), frameon=True, layout="constrained")
+    fig = plt.figure(
+        "Halstead metric", figsize=(10, 5), frameon=True, layout="constrained"
+    )
     plt.subplot(2, 1, 1)
     make_bar(
         fig,
         difficulties_files,
         summaries_files,
-        title='Halstead metric per file',
-        xlabel='Files',
-        ylabel='Difficulty'
+        title="Halstead metric per file",
+        xlabel="Files",
+        ylabel="Difficulty",
     )
-    make_plot_description(f"Total files count: {len(difficulties_files)}\n"
-                          f"Nonzero halstead metric in {np.count_nonzero(difficulties_files)} files")
+    make_plot_description(
+        f"Total files count: {len(difficulties_files)}\n"
+        f"Nonzero halstead metric in {np.count_nonzero(difficulties_files)} files"
+    )
     plt.subplot(2, 1, 2)
     make_bar(
         fig,
         difficulties_funcs,
         summaries_funcs,
-        title='Halstead metric per function',
-        xlabel='Functions',
-        ylabel='Difficulty'
+        title="Halstead metric per function",
+        xlabel="Functions",
+        ylabel="Difficulty",
     )
-    make_plot_description(f"Total functions count: {len(difficulties_funcs)}\n"
-                          f"Nonzero halstead metric in {np.count_nonzero(difficulties_funcs)} functions")
-    save_or_show(save, 'Halstead.png')
+    make_plot_description(
+        f"Total functions count: {len(difficulties_funcs)}\n"
+        f"Nonzero halstead metric in {np.count_nonzero(difficulties_funcs)} functions"
+    )
+    save_or_show(save_output, "Halstead.png")
 
 
-def raw_parser(data: dict, save=False):
+def raw_parser(data: dict, save_output: str = None):
     loc = []
     lloc = []
     sloc = []
@@ -211,62 +225,82 @@ def raw_parser(data: dict, save=False):
         multiline_strings.append(entity["multi"])
         blank.append(entity["blank"])
         labels.append(
-            f"filename: {filename}\n" +
-            "\n".join([f'{key}: {value}' for key, value in entity.items()])
+            f"filename: {filename}\n"
+            + "\n".join([f"{key}: {value}" for key, value in entity.items()])
         )
 
-    fig = plt.figure('Statistics', figsize=(10, 5), frameon=True, layout="constrained")
+    fig = plt.figure("Statistics", figsize=(10, 5), frameon=True, layout="constrained")
     make_stacked_bars(
         fig,
         values=(sloc, oneline_strings, multiline_strings, blank),
         labels=labels,
-        categories=("SLOC", "oneline comments and docstrings", "multiline docstrings", "blank"),
+        categories=(
+            "SLOC",
+            "oneline comments and docstrings",
+            "multiline docstrings",
+            "blank",
+        ),
         title="Raw",
         xlabel="files",
-        ylabel="lines of code"
+        ylabel="lines of code",
     )
-    fig.legend()
-    make_plot_description(f"Total files count: {len(files)}\n"
-                          f"LOC: {sum(loc)}\n"
-                          f"SLOC: {sum(sloc)}\n"
-                          f"Oneline comments and docstrings: {sum(oneline_strings)}\n"
-                          f"Multiline docstrings: {sum(multiline_strings)}\n"
-                          f"Blank: {sum(blank)}")
-    save_or_show(save, 'raw_statistics.png')
+    plt.legend(bbox_to_anchor=(1.1, 1.05), fancybox=True)
+    make_plot_description(
+        f"Total files count: {len(files)}\n"
+        f"LOC: {sum(loc)}\n"
+        f"SLOC: {sum(sloc)}\n"
+        f"Oneline comments and docstrings: {sum(oneline_strings)}\n"
+        f"Multiline docstrings: {sum(multiline_strings)}\n"
+        f"Blank: {sum(blank)}"
+    )
+    save_or_show(save_output, "raw_statistics.png")
 
-    fig = plt.figure('Statistics', figsize=(10, 5), frameon=True, layout="constrained")
+    fig = plt.figure("Statistics", figsize=(10, 5), frameon=True, layout="constrained")
     plt.subplot(2, 1, 1)
-    make_bar(fig, labels=labels, values=lloc, title="Logical lines of code", xlabel="files", ylabel="lines of code")
-    make_plot_description(f"Total files count: {len(files)}\n"
-                          f"LLOC: {sum(lloc)}")
+    make_bar(
+        fig,
+        labels=labels,
+        values=lloc,
+        title="Logical lines of code",
+        xlabel="files",
+        ylabel="lines of code",
+    )
+    make_plot_description(f"Total files count: {len(files)}\n" f"LLOC: {sum(lloc)}")
     plt.subplot(2, 1, 2)
-    make_bar(fig, labels=labels, values=comments, title="Comments", xlabel="files", ylabel="lines of code")
-    make_plot_description(f"Total files count: {len(files)}\n"
-                          f"Comments: {sum(comments)}\n")
-    save_or_show(save, 'raw_LLOC_Comments.png')
+    make_bar(
+        fig,
+        labels=labels,
+        values=comments,
+        title="Comments",
+        xlabel="files",
+        ylabel="lines of code",
+    )
+    make_plot_description(
+        f"Total files count: {len(files)}\n" f"Comments: {sum(comments)}\n"
+    )
+    save_or_show(save_output, "raw_LLOC_Comments.png")
 
 
-def mi_parser(data: dict, save=False):
-    descriptions, mi = zip(*[(f"file: {k}\nmi: {v['mi']}%", v["mi"]) for k, v in data.items()])
+def mi_parser(data: dict, save_output: str = None):
+    descriptions, mi = zip(
+        *[(f"file: {k}\nmi: {v['mi']}%", v["mi"]) for k, v in data.items()]
+    )
     inverted_mi = np.array(mi) - 100
-    fig = plt.figure('Maintainability index', figsize=(10, 4), frameon=True, layout="constrained")
-    make_bar(fig, values=inverted_mi, labels=descriptions, title="MI", xlabel="files", ylabel="Percentage", bottom=100.0)
-    plot_description = (f"Total files count: {len(descriptions)}\n" 
-                        f"MI≠100% in {np.count_nonzero(inverted_mi)} files")
+    fig = plt.figure(
+        "Maintainability index", figsize=(10, 4), frameon=True, layout="constrained"
+    )
+    make_bar(
+        fig,
+        values=inverted_mi,
+        labels=descriptions,
+        title="MI",
+        xlabel="files",
+        ylabel="Percentage",
+        bottom=100.0,
+    )
+    plot_description = (
+        f"Total files count: {len(descriptions)}\n"
+        f"MI≠100% in {np.count_nonzero(inverted_mi)} files"
+    )
     make_plot_description(plot_description)
-    save_or_show(save, 'mi.png')
-
-
-save_chart = False
-
-with open("raw_results.json", "r") as f:
-    raw_parser(json.loads(f.read()), save=save_chart)
-
-with open("cc_results.json", "r") as f:
-    cc_parser(json.loads(f.read()), save=save_chart)
-
-with open("hal_results.json", "r") as f:
-    hal_parser(json.loads(f.read()), save=save_chart)
-
-with open("mi_results.json", "r") as f:
-    mi_parser(json.loads(f.read()), save=save_chart)
+    save_or_show(save_output, "mi.png")
